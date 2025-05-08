4a334070a6dc3ef99eb45983467d447af495142c
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-05-08 21:51:19 +0700

    implementasi showdatepicker

--- lib/presentation/page.dart
+++ lib/presentation/page.dart
@@ -1,6 +1,6 @@
-// === [Commit 01] init: setup TodoPage widget structure ===
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart'; 
+import '../bloc/todo_bloc.dart'; 
 
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
@@ -21,7 +21,20 @@ class TodoPage extends StatelessWidget {
                     Text('Selected Date'),
                     SizedBox(height: 8.0),
                     ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2100),
+                        ).then((selectedDate) {
+                          if (selectedDate != null) {
+                            context.read<TodoBloc>().add(
+                              TodoSelectDate(date: selectedDate),
+                            );
+                          }
+                        });
+                      },
                       child: Text('Select Date'),
                     ),
                   ],
